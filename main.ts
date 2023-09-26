73110c29b920b7707deb8fa2f4bee5c85c1c6d71
Torgeir Bull <sirbull@gmail.com>
2023-09-26 11:23:21 +0200

    Fixed almost everthing.

--- main.ts
+++ main.ts
@@ -1,29 +1,87 @@
 namespace SpriteKind {
     export const keys = SpriteKind.create()
+    export const level = SpriteKind.create()
 }
+sprites.onOverlap(SpriteKind.Player, SpriteKind.level, function (sprite, otherSprite) {
+    if (gotKey == true) {
+        door.setImage(img`
+            ................................................
+            ................................................
+            ................................................
+            ................................................
+            ................................................
+            44...........................................444
+            ee44.......................................44eee
+            ee4e44...................................44ee4ee
+            c4eee444...............................44e4eee4c
+            fcce4eee444...........................44eee4eccf
+            .ffccce4eee444......................44ee4eeccff.
+            ...fffccce4eee4....................4ee4eeccff...
+            ......fffcce55e4..................4e55cccff.....
+            .........ffccce4..................4eccfff.......
+            ...........fffcf..................fcff..........
+            ..............ff..................ff............
+            `)
+        tiles.setWallAt(tiles.getTileLocation(13, 30), false)
+    } else {
+        tiles.setWallAt(tiles.getTileLocation(13, 30), true)
+    }
+})
 sprites.onOverlap(SpriteKind.Player, SpriteKind.keys, function (sprite, otherSprite) {
-    sprites.destroy(GoldKey)
     game.showLongText("You got the key!", DialogLayout.Bottom)
-    music.play(music.stringPlayable("C C5 - - - - - - ", 400), music.PlaybackMode.UntilDone)
-    Dudleif = sprites.create(img`
+    sprites.destroy(GoldKey)
+    Dudleif.setImage(img`
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
         . . . . . 4 4 4 4 4 4 4 . . . . 
         . . . 4 4 4 4 4 4 4 4 4 4 . . . 
-        . . 4 4 4 5 5 5 5 5 4 5 4 . . . 
-        . . . . 5 f f f f f f 5 . . . . 
-        . . . . 5 1 8 5 5 8 1 5 . . . . 
-        . . . . 5 5 5 5 5 5 5 5 . . . . 
-        . . . . 5 5 5 1 1 5 5 5 . . . . 
-        . . b b b 5 5 f f 5 5 5 2 . . . 
-        . . b 2 b b b b b 5 5 2 2 2 . . 
-        . . 5 2 b 2 b 2 b 2 2 2 2 5 . . 
-        . . b b b 2 2 2 2 2 2 2 2 2 . . 
+        . . 4 4 4 7 7 7 7 7 4 7 4 . . . 
+        . . . . 7 f f f f f f 7 . . . . 
+        . . . . 7 1 8 7 7 8 1 7 . . . . 
+        . . . . 7 7 7 7 7 7 7 7 . . . . 
+        . . . . 7 7 7 1 1 7 7 7 . . . . 
+        . . 5 5 5 7 7 f f 7 7 7 2 . . . 
+        . . 5 2 5 5 5 5 5 7 7 2 2 2 . . 
+        . . 7 2 5 2 5 2 5 2 2 2 2 7 . . 
+        . . 5 5 5 2 2 2 2 2 2 2 2 2 . . 
         . . . f f . . . . . . f f . . . 
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
-        `, SpriteKind.Player)
+        `)
+    gotKey = true
+    music.play(music.stringPlayable("C C5 - - - - - - ", 400), music.PlaybackMode.UntilDone)
 })
+scene.onOverlapTile(SpriteKind.Player, sprites.castle.tileDarkGrass2, function (sprite, location) {
+    game.gameOver(true)
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.stairSouth, function (sprite, location) {
+    if (gotKey == true) {
+        door.setImage(img`
+            44...........................................444
+            ee44.......................................44eee
+            ee4e44...................................44ee4ee
+            c4eee444...............................44e4eee4c
+            fcce4eee444...........................44eee4eccf
+            .ffccce4eee444......................44ee4eeccff.
+            ...fffccce4eee4....................4ee4eeccff...
+            ......fffcce55e4..................4e55cccff.....
+            .........ffccce4..................4eccfff.......
+            ...........fffcf..................fcff..........
+            ..............ff..................ff............
+            ................................................
+            ................................................
+            ................................................
+            ................................................
+            ................................................
+            `)
+        tiles.setWallAt(tiles.getTileLocation(13, 30), false)
+    } else {
+        Dudleif.sayText("I need a key...", 1000, true)
+        tiles.setWallAt(tiles.getTileLocation(13, 30), true)
+    }
+})
+let gotKey = false
+let door: Sprite = null
 let GoldKey: Sprite = null
 let Dudleif: Sprite = null
 tiles.setCurrentTilemap(tilemap`level1`)
@@ -32,20 +90,20 @@ Dudleif = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . 4 4 4 4 4 4 4 . . . . 
     . . . 4 4 4 4 4 4 4 4 4 4 . . . 
-    . . 4 4 4 5 5 5 5 5 4 5 4 . . . 
-    . . . . 5 f f f f f f 5 . . . . 
-    . . . . 5 1 8 5 5 8 1 5 . . . . 
-    . . . . 5 5 5 5 5 5 5 5 . . . . 
-    . . . . 5 5 5 1 1 5 5 5 . . . . 
-    . . . 2 5 5 5 f f 5 5 5 2 . . . 
-    . . 2 2 2 5 5 5 5 5 5 2 2 2 . . 
-    . . 5 2 2 2 2 2 2 2 2 2 2 5 . . 
+    . . 4 4 4 7 7 7 7 7 4 7 4 . . . 
+    . . . . 7 f f f f f f 7 . . . . 
+    . . . . 7 1 8 7 7 8 1 7 . . . . 
+    . . . . 7 7 7 7 7 7 7 7 . . . . 
+    . . . . 7 7 7 1 1 7 7 7 . . . . 
+    . . . 2 7 7 7 f f 7 7 7 2 . . . 
+    . . 2 2 2 7 7 7 7 7 7 2 2 2 . . 
+    . . 7 2 2 2 2 2 2 2 2 2 2 7 . . 
     . . 2 2 2 2 2 2 2 2 2 2 2 2 . . 
     . . . f f . . . . . . f f . . . 
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player)
-tiles.placeOnTile(Dudleif, tiles.getTileLocation(3, 2))
+tiles.placeOnTile(Dudleif, tiles.getTileLocation(4, 2))
 controller.moveSprite(Dudleif)
 scene.cameraFollowSprite(Dudleif)
 GoldKey = sprites.create(img`
@@ -67,3 +125,23 @@ GoldKey = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.keys)
 tiles.placeOnTile(GoldKey, tiles.getTileLocation(7, 13))
+door = sprites.create(img`
+    ................................................
+    ................................................
+    ...444444444444444444444f44444444444444444444...
+    ..4ceee4ceee4ceee4ceeeeefeeeeec4eeec4eec4eeec4..
+    .4ceee4ceee4ceee4cee455efe455eec4eeec4eec4eeec4.
+    4ceee4ceee4ceee4ceeeeeeefeeeeeeec4eeec4eec4eeec4
+    444444444444444444444444f44444444444444444444444
+    eeeeeeeeeeeeeeeeeeeeeeeefeeeeeeeeeeeeeeeeeeeeeee
+    ccccccccccccccccccccceefffeecccccccccccccccccccc
+    ffffffffffffffffffffffff.fffffffffffffffffffffff
+    ................................................
+    ................................................
+    ................................................
+    ................................................
+    ................................................
+    ................................................
+    `, SpriteKind.level)
+tiles.placeOnTile(door, tiles.getTileLocation(13, 30))
+gotKey = false
